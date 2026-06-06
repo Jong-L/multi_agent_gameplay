9d9f50f365103c53bc6fb1243625c575d7bf8dd8
Jong-L <Yiwei23214@163.com>
2026-06-06 21:39:51 +0800

    论文个人认为完成

--- .history/toy_20260518214238.py
+++
@@ -1,9 +0,0 @@
-import torch
-
-a = torch.tensor([1, 2, 3])
-b = torch.tensor([4, 5, 6])
-
-# cat: 沿现有维度拼接
-c = torch.cat([a, b])          # tensor([1, 2, 3, 4, 5, 6])
-c = torch.cat([a, b], dim=-1)
-print(c) 
